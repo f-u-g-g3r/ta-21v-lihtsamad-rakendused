0459770af25999c5eac8cd79eecac8753601aacd
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2022-03-24 11:28:00 +0200

    tictactoe saved

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -7,42 +7,155 @@ if next_position == "1":
     print(". . .")
     print(". . .")
 
-if next_position == "2":
+    next_position = input("Please type what position: ")
+
+    if next_position == "1":
+        print("CANT DO THE MOVE")
+
+    elif next_position == "2":
+        print("x o .")
+        print(". . .")
+        print(". . .")
+        
+        next_position = input("Please type what position: ")
+
+        if next_position == "1":
+            print("CANT DO THE MOVE")
+        elif next_position == "2":
+            print("CANT DO THE MOVE")
+
+        elif next_position == "3":
+            print("x o x")
+            print(". . .")
+            print(". . .")
+
+            next_position = input("Please type what position: ")
+            
+            if next_position == "1":
+                print("CANT DO THE MOVE")
+            elif next_position == "2":
+                print("CANT DO THE MOVE")
+            elif next_position == "3":
+                print("CANT DO THE MOVE")
+
+            elif next_position == "4":
+                print("x o x")
+                print("o . .")
+                print(". . .")
+
+                next_position = input("Please type what position: ")
+            
+                if next_position == "1":
+                    print("CANT DO THE MOVE")
+                elif next_position == "2":
+                    print("CANT DO THE MOVE")
+                elif next_position == "3":
+                    print("CANT DO THE MOVE")
+                elif next_position == "4":
+                    print("CANT DO THE MOVE")
+
+                elif next_position == "5":
+                    print("x o x")
+                    print("o x .")
+                    print(". . .")
+
+                    next_position = input("Please type what position: ")
+            
+                    if next_position == "1":
+                        print("CANT DO THE MOVE")
+                    elif next_position == "2":
+                        print("CANT DO THE MOVE")
+                    elif next_position == "3":
+                        print("CANT DO THE MOVE")
+                    elif next_position == "4":
+                        print("CANT DO THE MOVE")
+                    elif next_position == "5":
+                        print("CANT DO THE MOVE")
+
+                    elif next_position == "6":
+                        print("x o x")
+                        print("o x o")
+                        print(". . .")
+                        next_position = input("Please type what position: ")
+
+                    
+            
+                
+
+
+        
+        
+    elif next_position == "3":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "4":
+        print("x . .")
+        print("o . .")
+        print(". . .")
+    
+    elif next_position == "5":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "6":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "7":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "8":
+        print("x . o")
+        print(". . .")
+        print(". . .")
+
+    elif next_position == "9":
+        print("x . .")
+        print(". . .")
+        print(". . o")
+
+elif next_position == "2":
     print(". x .")
     print(". . .")
     print(". . .")
 
-if next_position == "3":
+elif next_position == "3":
     print(". . x")
     print(". . .")
     print(". . .")
 
-if next_position == "4":
+elif next_position == "4":
     print(". . .")
     print("x . .")
     print(". . .")
 
-if next_position == "5":
+elif next_position == "5":
     print(". . .")
     print(". x .")
     print(". . .")
 
-if next_position == "6":
+elif next_position == "6":
     print(". . .")
     print(". . x")
     print(". . .")
 
-if next_position == "7":
+elif next_position == "7":
     print(". . .")
     print(". . .")
     print("x . .")
 
-if next_position == "8":
+elif next_position == "8":
     print(". . .")
     print(". . .")
     print(". x .")
 
-if next_position == "9":
+elif next_position == "9":
     print(". . .")
     print(". . .")
     print(". . x")
