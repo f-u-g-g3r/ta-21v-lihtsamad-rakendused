5b3909785130b5edd30c45447d3ff8768485bc52
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-03-31 01:05:17 +0300

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -7,17 +7,27 @@ def showGame():
     print(game[0] + game[1] + game[2])
     print(game[3] + game[4] + game[5])
     print(game[6] + game[7] + game[8])
-i = 1
-while i < 10:
-    if i % 2 != 0:
-        next_position = int(input("Type what position: "))
-        game[next_position - 1] = "x"
+i = 0
+while i < 9:
+    if i % 2 == 0:
+        next_position = int(input("Position for player 1: "))
         
-        showGame()
+        if game[next_position - 1] != "x":
+            if game[next_position - 1] != "o":
+                game[next_position - 1] = "x"   
+                showGame()
+        else:
+            print("Error")     
+            break
     else:
-        next_position = int(input("Type what position: "))
-        game[next_position - 1] = "o"
-        showGame()
+        next_position = int(input("Position for player 2: "))
+        if game[next_position - 1] != "x":
+            if game[next_position - 1] != "o":
+                game[next_position - 1] = "o"   
+                showGame()
+        else:
+            print("Error")     
+            break
 
     if game[0] == "x" and game[1] == "x" and game[2] == "x" or game[3] == "x" and game[4] == "x" and game[5] == "x" or game[6] == "x" and game[7] == "x" and game[8] == "x" or game[0] == "x" and game[3] == "x" and game[6] == "x" or game[1] == "x" and game[4] == "x" and game[7] == "x" or game[2] == "x" and game[5] == "x" and game[8] == "x" or game[0] == "x" and game[4] == "x" and game[8] == "x" or game[2] == "x" and game[4] == "x" and game[6] == "x":
         print("you win")
@@ -26,5 +36,5 @@ while i < 10:
     if game[0] == "o" and game[1] == "o" and game[2] == "o" or game[3] == "o" and game[4] == "o" and game[5] == "o" or game[6] == "o" and game[7] == "o" and game[8] == "o" or game[0] == "o" and game[3] == "o" and game[6] == "o" or game[1] == "o" and game[4] == "o" and game[7] == "o" or game[2] == "o" and game[5] == "o" and game[8] == "o" or game[0] == "o" and game[4] == "o" and game[8] == "o" or game[2] == "o" and game[4] == "o" and game[6] == "o":
         print("you win")
         break
-    
+
     i += 1
