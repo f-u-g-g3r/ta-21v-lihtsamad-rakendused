04094a86362948ece9a58e3736425d5ed4c1873b
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-03-30 21:36:58 +0300

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -12,11 +12,19 @@ while i < 10:
     if i % 2 != 0:
         next_position = int(input("Type what position: "))
         game[next_position - 1] = "x"
+        
         showGame()
     else:
         next_position = int(input("Type what position: "))
         game[next_position - 1] = "o"
         showGame()
-    i += 1
-        
 
+    if game[0] == "x" and game[1] == "x" and game[2] == "x" or game[3] == "x" and game[4] == "x" and game[5] == "x" or game[6] == "x" and game[7] == "x" and game[8] == "x" or game[0] == "x" and game[3] == "x" and game[6] == "x" or game[1] == "x" and game[4] == "x" and game[7] == "x" or game[2] == "x" and game[5] == "x" and game[8] == "x" or game[0] == "x" and game[4] == "x" and game[8] == "x" or game[2] == "x" and game[4] == "x" and game[6] == "x":
+        print("you win")
+        break
+
+    if game[0] == "o" and game[1] == "o" and game[2] == "o" or game[3] == "o" and game[4] == "o" and game[5] == "o" or game[6] == "o" and game[7] == "o" and game[8] == "o" or game[0] == "o" and game[3] == "o" and game[6] == "o" or game[1] == "o" and game[4] == "o" and game[7] == "o" or game[2] == "o" and game[5] == "o" and game[8] == "o" or game[0] == "o" and game[4] == "o" and game[8] == "o" or game[2] == "o" and game[4] == "o" and game[6] == "o":
+        print("you win")
+        break
+    
+    i += 1
