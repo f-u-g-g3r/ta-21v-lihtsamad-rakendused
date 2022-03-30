11d8dd11be719d46b7506bdeb33ce1d115bdf208
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-03-30 17:34:36 +0300

    Update 2_tictactoe.py

--- 2_tictactoe.py
+++ 2_tictactoe.py
@@ -3,14 +3,20 @@ game = [
     ".", ".", ".",
     ".", ".", "."
 ]
-print(game[0] + game[1] + game[2])
-print(game[3] + game[4] + game[5])
-print(game[6] + game[7] + game[8])
-
-next_position = input("Type what position: ")
-
-if next_position == "1":
-    game[0] = "x"
+def showGame():
     print(game[0] + game[1] + game[2])
     print(game[3] + game[4] + game[5])
     print(game[6] + game[7] + game[8])
+i = 1
+while i < 10:
+    if i % 2 != 0:
+        next_position = int(input("Type what position: "))
+        game[next_position - 1] = "x"
+        showGame()
+    else:
+        next_position = int(input("Type what position: "))
+        game[next_position - 1] = "o"
+        showGame()
+    i += 1
+        
+
