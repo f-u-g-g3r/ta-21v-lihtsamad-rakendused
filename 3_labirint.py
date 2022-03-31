65d2a8fe47e2dbe6c2c88a6a896bab56eaa10b25
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2022-03-31 10:37:56 +0300

    Update 3_labirint.py

--- 3_labirint.py
+++ 3_labirint.py
@@ -1,9 +1,9 @@
 map = [
     [12, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 0],
-    [1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 24]
+    [1, 1, 1, 0, 1],
+    [0, 0, 1, 1, 0],
+    [24, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1]
 ]
 
 start_pos_x = 0
@@ -17,14 +17,24 @@ def get_next_free_position(current_position_y, current_position_x):
         print("can go right")
         return [current_position_y, current_position_x + 1]
 
+    if current_position_x - 1 < 0 and map[current_position_y][current_position_x - 1] == 1:
+        print("can go left")
+        return [current_position_y, current_position_x - 1]
+
     if current_position_y + 1 < y_rows and map[current_position_y + 1][current_position_x] == 1:
         print("can go bottom")
         return [current_position_y + 1, current_position_x]
+
+    if current_position_y - 1 < 0 and map[current_position_y - 1][current_position_x] == 1:
+        print("can go up")
+        return [current_position_y - 1, current_position_x]
+    
 
 next_free_position = get_next_free_position(start_pos_x, start_pos_y)
 print("Next free position is: ", next_free_position)
 while next_free_position:
     next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
     print("Next free position is: ", next_free_position)
+    
 
 
