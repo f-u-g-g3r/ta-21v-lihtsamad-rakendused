df0f5591ab1f425a863eaea49373a4a1b135a82b
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2022-03-31 09:52:30 +0300

    Update 3_labirint.py

--- 3_labirint.py
+++ 3_labirint.py
@@ -8,9 +8,14 @@ map = [
 
 start_pos_x = 0
 start_pos_y = 0
+def get_next_free_position():
+    if map[start_pos_x][start_pos_y + 1] == 1:
+        print("can go right")
+        return [start_pos_y, start_pos_x + 1]
 
-if map[start_pos_x][start_pos_y + 1] == 1:
-    print("can go right")
+    if map[start_pos_x + 1][start_pos_y] == 1:
+        print("can go bottom")
+        return [start_pos_y + 1, start_pos_x]
 
-if map[start_pos_x + 1][start_pos_y] == 1:
-    print("can go bottom")
+next_free_position = get_next_free_position()
+print("Next free position is: ", next_free_position)
