e2dbc47bad2be593a84733ae7c4f7ca0f1188003
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2022-04-07 10:05:38 +0300

    Update 4_labirint.py

--- 4_labirint.py
+++ 4_labirint.py
@@ -25,10 +25,15 @@ def get_next_free_position(current_position_y, current_position_x):
     can_go_left = current_position_x - 1 > 0 and map[current_position_y][current_position_x - 1] == ROAD
     can_go_bottom = current_position_y + 1 < y_rows and map[current_position_y + 1][current_position_x] == ROAD
     can_go_top = current_position_y - 1 > 0 and map[current_position_y - 1][current_position_x] == ROAD
-    can_finish = current_position_y + 1 < y_rows and map[current_position_y + 1][current_position_x] == FINISH
+
+    finish_go_right = current_position_x + 1 < x_columns and map[current_position_y][current_position_x + 1] == FINISH
+    finish_go_left = current_position_x - 1 > 0 and map[current_position_y][current_position_x - 1] == FINISH
+    finish_go_bottom = current_position_y + 1 < y_rows and map[current_position_y + 1][current_position_x] == FINISH
+    finish_go_top = current_position_y - 1 > 0 and map[current_position_y - 1][current_position_x] == FINISH
 
 
     available_next_positions = []
+
 
     if can_go_left:
         print("can go left")
@@ -58,4 +63,4 @@ print("Next free position is: ", next_free_position)
 while next_free_position:
     next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
     print("Next free position is: ", next_free_position)
-    if 
+    
