53839ca92aa7ab3ac0d10b88b646f78c305124d5
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2022-04-07 09:33:36 +0300

    Create 4_labirint.py

---
+++ 4_labirint.py
@@ -0,0 +1,89 @@
+import random
+START = 1
+FINISH = 0
+ROAD = 1
+WALL = 0
+
+map = [
+    [START, ROAD, ROAD, ROAD, ROAD],
+    [WALL, WALL, WALL, WALL, ROAD],
+    [WALL, WALL, WALL, WALL, ROAD],
+    [WALL, WALL, WALL, WALL, ROAD],
+    [WALL, WALL, WALL, WALL, FINISH],
+]
+
+start_pos_x = 0
+start_pos_y = 0
+
+y_rows = 5
+x_columns = 5
+
+def get_next_free_position(current_position_y, current_position_x):
+    random_number = random.random()
+
+    can_go_right = current_position_x + 1 < x_columns and map[current_position_y][current_position_x + 1] == 1
+    can_go_left = current_position_x - 1 >= 0 and map[current_position_y][current_position_x - 1] == 1
+    can_go_bottom = current_position_y + 1 < y_rows and map[current_position_y + 1][current_position_x] == 1
+    can_go_top = current_position_y - 1 >= 0 and map[current_position_y - 1][current_position_x] == 1 or 24
+    
+    if  can_go_top and can_go_bottom and can_go_right:
+        if random_number < 0.25 :
+            print("can go bottom")
+            return [current_position_y + 1, current_position_x]
+        elif random_number > 0.25 and random_number < 0.75:
+            print("can go right")
+            return [current_position_y, current_position_x + 1]
+        elif random_number > 0.75:
+            print("can go top")
+            return [current_position_y - 1, current_position_x]
+
+    if can_go_left and can_go_right:
+        if random_number < 0.5 :
+            print("can go left")
+            return [current_position_y, current_position_x - 1]
+        elif random_number > 0.5:
+            print("can go right")
+            return [current_position_y, current_position_x + 1]
+    
+    if can_go_left and can_go_bottom:
+        if random_number < 0.5 :
+            print("can go left")
+            return [current_position_y, current_position_x - 1]
+        elif random_number > 0.5:
+            print("can go bottom")
+            return [current_position_y + 1, current_position_x]
+            
+    if can_go_right and can_go_top:
+        if random_number < 0.5:
+            print("can go right")
+            return [current_position_y, current_position_x + 1]
+        elif random_number > 0.5:
+            print("can go top")
+            return [current_position_y - 1, current_position_x]
+
+    if can_go_left:
+        print("can go left")
+        return [current_position_y, current_position_x - 1]
+
+    if can_go_right:
+        print("can go right")
+        return [current_position_y, current_position_x + 1]
+
+    if can_go_bottom:
+        print("can go bottom")
+        return [current_position_y + 1, current_position_x]
+
+    if can_go_top:
+        print("can go up")
+        return [current_position_y - 1, current_position_x]
+    
+
+next_free_position = get_next_free_position(start_pos_x, start_pos_y)
+print("Next free position is: ", next_free_position)
+
+while next_free_position:
+    next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+    print("Next free position is: ", next_free_position)
+
+    if next_free_position == [3, 0]:
+        break
