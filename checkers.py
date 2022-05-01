cdba06ce38ece85e39d1a30ef5de62844fb559b3
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-01 03:04:12 +0300

    Update checkers.py

--- checkers.py
+++ checkers.py
@@ -1,8 +1,18 @@
 import cv2
-import numpy as np
+import numpy
 import mediapipe
 
 colorBlack = (0, 0, 0)
 colorWhite = (255, 255, 255)
+colorLightBrown = (140, 140, 140)
+colorDarkBrown = (200, 200, 200)
+thickness = 1
+img = numpy.zeros((512, 512))
+posZeroZero = (0, 0)
 
-img = np.zeros((512,512,3), np.uint8)
+while True:
+    cv2.imshow('Checkers', img)
+    cv2.rectangle(img, (0, 0), (52, 52), colorLightBrown, thickness)
+    cv2.rectangle(img, (0, 0), (52, 52), colorLightBrown, thickness)
+    if cv2.waitKey(1) == ord('q'):
+        break
