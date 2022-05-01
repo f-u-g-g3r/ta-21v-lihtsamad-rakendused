b43ed0719068e9eb99f93cf2605d9bf115d63b7e
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-01 03:18:05 +0300

    Update checkers.py

--- checkers.py
+++ checkers.py
@@ -8,11 +8,21 @@ colorLightBrown = (140, 140, 140)
 colorDarkBrown = (200, 200, 200)
 thickness = 1
 img = numpy.zeros((512, 512))
-posZeroZero = (0, 0)
+pos52_0 = [52, 0]
+pos52_52 = [52, 52]
 
+def DrawPlace():
+    for i in range(0, 512, 52):
+        cv2.rectangle(img, pos52_0, pos52_52, colorLightBrown, thickness)
+        pos52_52[0] = pos52_52[0] + 52
+DrawPlace()
 while True:
     cv2.imshow('Checkers', img)
-    cv2.rectangle(img, (0, 0), (52, 52), colorLightBrown, thickness)
-    cv2.rectangle(img, (52, 0), (104, 52), colorLightBrown, thickness)
+    DrawPlace()
+    # cv2.rectangle(img, (0, 0), (52, 52), colorLightBrown, thickness)
+    # cv2.rectangle(img, (52, 0), (104, 52), colorLightBrown, thickness)
+    # cv2.rectangle(img, (52, 0), (156, 52), colorLightBrown, thickness)
+    # cv2.rectangle(img, (52, 0), (156, 52), colorLightBrown, thickness)
+    # cv2.rectangle(img, (52, 0), (208, 52), colorLightBrown, thickness)
     if cv2.waitKey(1) == ord('q'):
         break
