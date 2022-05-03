4095cf2bce8ec0224d56956be7de38faf740ee01
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-04 01:06:07 +0300

    Update viselitsa.py

--- viselitsa.py
+++ viselitsa.py
@@ -7,13 +7,21 @@ wordToGuess = random.choice(words)
 letters = ["_"] * len(wordToGuess)
 print(letters)
 
-lives = 4
+lives = 5
 
 def drawGameField():
     if(lives == 4):
-        print("Г---------")
+        print("Г------------")
+        print("|            ")
+        print("|            ")
+        print("|            ")
+        print("|            ")
+        print("|            ")
+    elif(lives == 3):
+        print("Г------------")
+        print("|       |    ")
+        print("|       O    ")
         print("|")
         print("|")
         print("|")
-        print("|")
-        print("|")
+
