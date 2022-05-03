b06d3a3c83d6a1c37824af22ace0bccb88097689
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-04 02:35:48 +0300

    Update checkers.py

--- checkers.py
+++ checkers.py
@@ -93,29 +93,104 @@ def checkSecondIndex(checker_char_position):
             print("Wrong checker_char_position")
             break
 
+def checkNewFirstIndex(next_checker_num):
+    while True:
+        if next_checker_num == "1":
+            firstIndex = 7
+            return firstIndex
+        elif next_checker_num == "2":
+            firstIndex = 6
+            return firstIndex
+        elif next_checker_num == "3":
+            firstIndex = 5
+            return firstIndex
+        elif next_checker_num == "4":
+            firstIndex = 4
+            return firstIndex
+        elif next_checker_num == "5":
+            firstIndex = 3
+            return firstIndex
+        elif next_checker_num == "6":
+            firstIndex = 2
+            return firstIndex
+        elif next_checker_num == "7":
+            firstIndex = 1
+            return firstIndex
+        elif next_checker_num == "8":
+            firstIndex = 0
+            return firstIndex
+        else:
+            print("Wrong checker_num_position")
+            break
+
+def checkNewSecondIndex(next_checker_char):
+    while True:
+        if next_checker_char == "a":
+            new_secondIndex = 0
+            return new_secondIndex
+        elif next_checker_char == "b":
+            new_secondIndex = 1
+            return new_secondIndex
+        elif next_checker_char == "c":
+            new_secondIndex = 2
+            return new_secondIndex
+        elif next_checker_char == "d":
+            new_secondIndex = 3
+            return new_secondIndex
+        elif next_checker_char == "e":
+            new_secondIndex = 4
+            return new_secondIndex
+        elif next_checker_char == "f":
+            new_secondIndex = 5
+            return new_secondIndex
+        elif next_checker_char == "g":
+            new_secondIndex = 6
+            return new_secondIndex
+        elif next_checker_char == "h":
+            new_secondIndex = 7
+            return new_secondIndex
+        else:
+            print("Wrong checker_char_position")
+            break
+
+def checkCanGoNext():
+    if field1[checkFirstIndex(checker_num_position)][checkSecondIndex(checker_char_position)] == " W ":
+        old_check_index_1 = [checkFirstIndex(checker_num_position)]
+        old_check_index_2 = [checkSecondIndex(checker_char_position)]
+        print(old_check_index_1, old_check_index_2)
+
+        next_checker_num = input("(White player) Choose next checker num position: ")
+        next_checker_char = input("(White player) Choose next checker char position: ")
+
+        if field1[checkFirstIndex(next_checker_num)][checkSecondIndex(next_checker_char)] == " + ":
+            new_check_index_1 = [checkFirstIndex(checker_num_position)]
+            new_check_index_2 = [checkSecondIndex(checker_char_position)]
+            print(new_check_index_1, new_check_index_2)
 while True:
     for i in range(0, 999999999):
         if i % 2 == 0:
             checker_num_position = input("(White player) Choose checker num position: ")
             checker_char_position = input("(White player) Choose checker char position: ")
-
-            if field1[checkFirstIndex(checker_num_position)][checkSecondIndex(checker_char_position)] == " W ":
-                checkerIndex = [checkFirstIndex(checker_num_position)] + [checkSecondIndex(checker_char_position)]
-                print(checkerIndex)
-                checker_num_position = input("(White player) Choose next checker num position: ")
-                checker_char_position = input("(White player) Choose next checker char position: ")
-                if field1[checkFirstIndex(checker_num_position)][checkSecondIndex(checker_char_position)] == " + ":
-                    newCheckerIndex = [checkFirstIndex(checker_num_position)] + [checkSecondIndex(checker_char_position)]
-                    newCheckerIndex[0] += 1 
-                    newCheckerIndex[1] -= 1
-                    if newCheckerIndex == checkerIndex:
-                        print("URA")
-                    
-                #if next_num_position[checkFirstIndex(nex)]
-            else:
-                print("You typed wrong!")
-                break
+            checkCanGoNext()
             
         else:
             next_position = int(input("Position for black player: "))
 
+
+
+# if field1[checkFirstIndex(checker_num_position)][checkSecondIndex(checker_char_position)] == " W ":
+            #     checkerIndex = [checkFirstIndex(checker_num_position)] + [checkSecondIndex(checker_char_position)]
+            #     print(checkerIndex)
+            #     checker_num_position = input("(White player) Choose next checker num position: ")
+            #     checker_char_position = input("(White player) Choose next checker char position: ")
+            #     if field1[checkFirstIndex(checker_num_position)][checkSecondIndex(checker_char_position)] == " + ":
+            #         newCheckerIndex = [checkFirstIndex(checker_num_position)] + [checkSecondIndex(checker_char_position)]
+            #         newCheckerIndex[0] += 1 
+            #         newCheckerIndex[1] -= 1
+            #         if newCheckerIndex == checkerIndex:
+            #             field1[checkerIndex] = " + "
+            #             field1[newCheckerIndex] = " W "
+                        
+            #             showField()
+                    
+                #if next_num_position[checkFirstIndex(nex)]
