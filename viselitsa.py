93e6e98ea894961b44a8c088463740ee04356822
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-04 02:18:00 +0300

    Update viselitsa.py

--- viselitsa.py
+++ viselitsa.py
@@ -1,6 +1,6 @@
 import random
 
-words = ["cat"] #"mathematic", "games", "computer", "elephant", "estonia"
+words = ["cat", "mathematic", "games", "computer", "elephant", "estonia", "dogs", "apple", "money", "rat", "dinosaur", "logic", "shark", "banana", "monkey"] 
 
 word_to_guess = random.choice(words)
 
@@ -10,6 +10,8 @@ for i in word_to_guess:
 
 letters = ["_"] * word_length
 lives = 8
+print(letters)
+
 
 def drawGameField():
     if(lives == 7):
@@ -71,14 +73,33 @@ def drawGameField():
 
 
 while("_" in letters):
-    print(letters)
     drawGameField()
-    try_index = int(input("Enter an index for a letter: "))
+
+    if lives == 0:
+        print("You lost all your lives, game over!")
+        break
+
+    try:
+        try_index = int(input("Enter a letter position you want to guess: "))
+    except:
+        print("You have to type a number")
+        continue
+
     if try_index > word_length:
         print("Out of range")
-    try_letter = input("Enter a letter: ")
-    if try_letter in word_to_guess[try_index]:
-        print("1")
+        continue
 
+    try_letter = input("Enter a letter: ")
+
+    if try_letter in word_to_guess[try_index - 1]:
+        letters[try_index - 1] = try_letter
+        print(letters)
+    else:
+        lives -= 1
+        print("Wrong letter")
+        print(letters)
+
+if "_" not in letters:
+    print("You won!")
 
 
