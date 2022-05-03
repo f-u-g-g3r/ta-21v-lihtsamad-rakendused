c5e6bd438d549de928dae85be0dc439a368f30db
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-05-04 01:00:42 +0300

    Update viselitsa.py

--- viselitsa.py
+++ viselitsa.py
@@ -3,4 +3,6 @@ import random
 words = ["cat", "mathematic", "games", "computer", "elephant", "estonia"]
 
 wordToGuess = random.choice(words)
-print(wordToGuess)
+
+letters = ["_"] * len(words)
+print(letters)
