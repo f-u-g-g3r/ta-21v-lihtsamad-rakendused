4adcfff16945a96de59b71353be4360b55ec46db
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-06-01 00:07:50 +0300

    Update 2June.py

--- 2June.py
+++ 2June.py
@@ -6,15 +6,18 @@ path = 'C:'
 showMemory = psutil.virtual_memory()
 diskSpace = shutil.disk_usage(path)
 
-diskSpacePercents = diskSpace[1] / diskSpace[0] # 1 = used, 0 = total
+diskSpacePercents = round(diskSpace[1] / diskSpace[0] * 100, 1) # 1 = used, 0 = total
 
 with open("memoryCheck.txt", "a") as textFile:
     textFile.write("Free RAM:   " + str(showMemory[4]) + "\n") # 4 = free
     textFile.write("Total RAM: " + str(showMemory[0]) + "\n"*2) # 0 = total
     textFile.write("Free memory on disk: " + str(diskSpace[2]) + "\n") # 2 = free
     textFile.write("Total memory on disk: " + str(diskSpace[0]) + "\n"*2) # 0 = total
+    
     if showMemory[2] > 20:
-        textFile.write("Your RAM is used over 20%! It is: " + str(showMemory[2]) + "%") # 2 = percents
+        textFile.write("Your RAM is used over 20%! It is: " + str(showMemory[2]) + "%" + "\n") # 2 = percents
+    if diskSpacePercents > 20:
+        textFile.write("Your disk space is used over 20%! It is: " + str(diskSpacePercents) + "%" +  "\n")
     #if diskSpace
 
 # print("Free RAM:   ", showMemory[4])
@@ -22,7 +25,6 @@ with open("memoryCheck.txt", "a") as textFile:
 
 # print("Free memory on disk:  ", diskSpace[2])
 # print("Total memory on disk: ", diskSpace[0])
-print(diskSpace)
 
 
 
