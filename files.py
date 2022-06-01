aea1bc6169dd6cc10991c82cc1185457e5c41ffa
f-u-g-g3r <artjom.kuznetsov1733@gmail.com>
2022-06-01 17:15:28 +0300

    Delete files.py

--- files.py
+++
@@ -1,9 +0,0 @@
-import os
-file_path = input("Enter file path: ")
-news = input("Type any text: ")
-
-if os.path.exists(file_path):
-    print("file exists succesfully")
-else:
-    print("file dosn't exist")
-
