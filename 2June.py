9faf1af8acf4bcb0d374824b77470e27db73c2c6
f-u-g-g3r <102284163+f-u-g-g3r@users.noreply.github.com>
2022-06-01 17:28:26 +0300

    Update 2June.py

--- 2June.py
+++ 2June.py
@@ -1,6 +1,8 @@
 
 import psutil
 import shutil
+import json
+import pip._vendor.requests
 path = 'C:'
 
 showMemory = psutil.virtual_memory()
@@ -8,23 +10,27 @@ diskSpace = shutil.disk_usage(path)
 
 diskSpacePercents = round(diskSpace[1] / diskSpace[0] * 100, 1) # 1 = used, 0 = total
 
+# Weather API
+api_key = "48e84370663b15b4520231f167f6262b"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+city_name = "Tallinn"
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+response = pip._vendor.requests.get(complete_url)
+x = response.json()
+if x["cod"] != "404":
+    y = x["main"]
+    current_temperature = y["temp"]
+    z = x["weather"]
+
 with open("memoryCheck.txt", "a") as textFile:
     textFile.write("Free RAM:   " + str(showMemory[4]) + "\n") # 4 = free
     textFile.write("Total RAM: " + str(showMemory[0]) + "\n"*2) # 0 = total
     textFile.write("Free memory on disk: " + str(diskSpace[2]) + "\n") # 2 = free
     textFile.write("Total memory on disk: " + str(diskSpace[0]) + "\n"*2) # 0 = total
-    
+    textFile.write("Temperature in Tallinn = " + str(round(current_temperature - 273.15, 1)))
     if showMemory[2] > 20:
         textFile.write("Your RAM is used over 20%! It is: " + str(showMemory[2]) + "%" + "\n") # 2 = percents
     if diskSpacePercents > 20:
         textFile.write("Your disk space is used over 20%! It is: " + str(diskSpacePercents) + "%" +  "\n")
-    #if diskSpace
-
-# print("Free RAM:   ", showMemory[4])ф
-# print("Totatl RAM: ", showMemory[0])
-
-# print("Free memory on disk:  ", diskSpace[2])
-# print("Total memory on disk: ", diskSpace[0])
-
 
 
